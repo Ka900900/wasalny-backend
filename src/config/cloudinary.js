28f84b8eac691e0b4d86c7ae23ece9fc342cc5b6
Karim <karim@wasalny.local>
2026-07-21 10:09:00 +0300

    fix: switch to root cloudinary credentials

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -10,7 +10,7 @@ const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
   cloud_name: 'daxl7bn0m',
-  api_key: '2112358381772155',
+  api_key: '112358381772155',
   api_secret: 'cT06wH9S8xVbZhaYiO2tEGzVWBE',
 });
 
